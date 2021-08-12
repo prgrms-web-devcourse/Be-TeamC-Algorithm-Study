a36b1cee2acaf093fe730f0aee16d6db0c8073aa
suebeen <1003jamie@naver.com>
2021-08-12 18:25:57 +0900

    Update 수빈-전화번호 목록.java

--- 해시/수빈-전화번호 목록.java
+++ 해시/수빈-전화번호 목록.java
@@ -1,20 +1,38 @@
-import java.util.HashMap;
+// import java.util.HashMap;
+
+// class Solution {
+//     public boolean solution(String[] phone_book) {
+        
+//         HashMap<String, Integer> hm = new HashMap<>();
+
+//         for(String phone: phone_book) {
+//             hm.put(phone, 0);
+//         }
+        
+//         for(String phone: phone_book) {
+//             for(int i=1; i<phone.length(); i++) {
+//                 String num = phone.substring(0, i);
+//                 if(hm.containsKey(num)) {
+//                     return false;
+//                 }
+//             }
+//         }
+
+//         return true;
+//     }
+// }
+
+// HashSet 사용 버전
+import java.util.*;
 
 class Solution {
     public boolean solution(String[] phone_book) {
         
-        HashMap<String, Integer> hm = new HashMap<>();
-
-        for(String phone: phone_book) {
-            hm.put(phone, 0);
-        }
+        Set <String> hs = new HashSet<>(Arrays.asList(phone_book));
         
-        for(String phone: phone_book) {
-            for(int i=1; i<phone.length(); i++) {
-                String num = phone.substring(0, i);
-                if(hm.containsKey(num)) {
-                    return false;
-                }
+        for(String key: hs) {
+            for(int i=1; i<key.length(); i++) {
+                if(hs.contains(key.substring(0, i))) return false;
             }
         }
 
