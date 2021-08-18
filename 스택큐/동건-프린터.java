00001527bea1ea146d124cbb7f38873c46d654a1
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-08-18 17:41:10 +0900

    Update 동건-프린터.java

--- 스택큐/동건-프린터.java
+++ 스택큐/동건-프린터.java
@@ -1,1 +1,53 @@
+import java.util.*;
+class Solution {
+    public int solution(int[] priorities, int location) {
+		Queue<Print> queue = new LinkedList<>();
 
+		for (int i = 0; i < priorities.length; i++) {
+			queue.offer(new Print(priorities[i], i));
+		}
+
+		int answer = 0;
+		while (!queue.isEmpty()) {
+			boolean check = false;
+			int currentPrint = queue.peek().getPriority();
+
+			for (Print print : queue) {
+				if (currentPrint < print.getPriority()) {
+					check = true;
+                    break;
+				}
+			}
+
+			if (check) {
+				queue.offer(queue.poll());
+			} else {
+				if (queue.poll().getLocation() == location) {
+					answer = priorities.length - queue.size();
+				}
+			}
+
+		}
+
+		return answer;
+	}
+
+	class Print {
+		private int priority;
+		private int location;
+
+		public Print(int priority, int location) {
+			this.priority = priority;
+			this.location = location;
+		}
+
+		public int getPriority() {
+			return priority;
+		}
+
+		public int getLocation() {
+			return location;
+		}
+
+	}
+}
