53a57db8c10ffa4803b74cc8c7df13bfdf85fc2e
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-09-09 17:36:40 +0900

    Create 동건-카펫.java

---
+++ 완전탐색/동건-카펫.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int[] solution(int brown, int yellow) {
+        int sum = brown + yellow;
+
+        for (int row = sum; row >= 3; row--) {
+            if (sum % row == 0) {
+                int col = sum / row;
+
+                int doYellow = (col - 2) * (row - 2);
+
+                if (yellow == doYellow) {
+                    return new int[]{row, col};
+                }
+
+            }
+        }
+        return new int[]{0,0};
+    }
+}
