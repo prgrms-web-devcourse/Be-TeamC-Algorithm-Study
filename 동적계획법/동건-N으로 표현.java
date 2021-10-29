0e074cb111241e02e52126d5502a1d4142a6f342
DongGeon0908 <50691225+DongGeon0908@users.noreply.github.com>
2021-10-29 10:14:08 +0900

    Update 동건-N으로 표현.java

--- 동적계획법/동건-N으로 표현.java
+++ 동적계획법/동건-N으로 표현.java
@@ -3,12 +3,9 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
-public class Main {
-    public static void main(String[] args) {
-        int N = 5;
-        int number = 12;
-
-        // N이 1개인 집합부터 8개인 집합까지 생성
+class Solution {
+    public int solution(int N, int number) {
+// N이 1개인 집합부터 8개인 집합까지 생성
         List<Set<Integer>> data = new ArrayList<>();
 
         // 초기화 진행
@@ -27,24 +24,30 @@ public class Main {
             // N을 i번 사용 (i는 실제로 1개부터!)
             str = str + N;
             data.get(i).add(Integer.parseInt(str));
+            
             for (int j = 0; j < i; j++) {
+                
+                // 사칙 연산하고 data에 삽입하기
                 for (int k : data.get(j)) {
                     for (int l : data.get(i - j - 1)) {
                         data.get(i).add(k + l);
                         data.get(i).add(k - l);
                         data.get(i).add(k * l);
+                        
+                        // 나누기 연산때 분모가 0이되면 exception이 터짐... 이것 때문에 하루 날림...
                         if (l != 0) {
                             data.get(i).add(k / l);
                         }
                     }
                 }
             }
+            // data 집합에 number가 존재하면 break;
             if (data.get(i).contains(number)) {
-                System.out.println(i+);
-                break;
+                // i를 0부터 돌리니까 +1 해주기
+                return i+1;
             }
         }
-        System.out.println(-1);
-
+        // 실패
+        return -1;
     }
 }
