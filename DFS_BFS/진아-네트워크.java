b16cc6f62f8e62fa009ac88c1782f513cdedecc0
nasaoreo <81736440+nasaoreo@users.noreply.github.com>
2021-11-16 17:28:44 +0900

    Update 진아-네트워크.java
    - visit 메소드 한정자 변경
    - 매개변수(n) 사용하도록 코드 변경

--- DFS_BFS/진아-네트워크.java
+++ DFS_BFS/진아-네트워크.java
@@ -1,9 +1,9 @@
 class Solution {
     public int solution(int n, int[][] computers) {
         int networkCnt = 0;
-        boolean[] isVisited = new boolean[computers.length];
+        boolean[] isVisited = new boolean[n];
 
-        for(int i=0;i<computers.length;i++){
+        for(int i=0;i<n;i++){
             if(!isVisited[i]){
                 networkCnt++;
                 visit(i, computers, isVisited);
@@ -13,7 +13,7 @@ class Solution {
         return networkCnt;
     }
 
-    public void visit(int i, int[][] computers, boolean[] isVisited){
+    private void visit(int i, int[][] computers, boolean[] isVisited){
         isVisited[i] = true;
 
         for(int j=0; j<computers.length; j++){
