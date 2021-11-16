7200949cfe1c90394a2716cbe1ed60694a723b63
nasaoreo <81736440+nasaoreo@users.noreply.github.com>
2021-11-16 17:25:33 +0900

    Update 진아-네트워크.java
    전역 변수 제거

--- DFS_BFS/진아-네트워크.java
+++ DFS_BFS/진아-네트워크.java
@@ -1,26 +1,24 @@
 class Solution {
-    private boolean[] isVisited;
-
     public int solution(int n, int[][] computers) {
         int networkCnt = 0;
-        this.isVisited = new boolean[n];
+        boolean[] isVisited = new boolean[computers.length];
 
-        for(int i=0;i<n;i++){
+        for(int i=0;i<computers.length;i++){
             if(!isVisited[i]){
                 networkCnt++;
-                visit(i, computers);
+                visit(i, computers, isVisited);
             }
         }
 
         return networkCnt;
     }
 
-    public void visit(int i, int[][] computers){
+    public void visit(int i, int[][] computers, boolean[] isVisited){
         isVisited[i] = true;
 
         for(int j=0; j<computers.length; j++){
             if(computers[i][j] == 1 && !isVisited[j]){
-                visit(j, computers);
+                visit(j, computers, isVisited);
             }
         }
     }
